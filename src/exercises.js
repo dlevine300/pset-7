56a52b6bb9a0d731bde5d06930db127f3bf16227
David Levine <dlevine300@users.noreply.github.com>
2020-01-13 10:45:14 -0500

    Ich möchte begehen

--- src/exercises.js
+++ src/exercises.js
@@ -2,9 +2,9 @@ function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
   }
-if (a[0] === b[0] || a.length-1 === b.length-1 ) {
-  return true;
-}
+  if (a[0] === b[0] || (a.length-1) === (b.length-1) ) {
+    return true;
+  }
 };
 function endsMeet(values, n) {
   // write your code here
